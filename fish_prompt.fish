3a04b35ee34396b019c4388deb62ad9f518f325e
Pan Pernicek <61617356+Pan00Pernicek@users.noreply.github.com>
2020-10-10 12:08:59 +0200

    Update fish_prompt.fish

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,3 +1,3 @@
 function fish_prompt
-        echo (tput setab 1)(prompt_pwd)(tput sgr0)' '(tput setaf 9)'><>'(tput sgr0)' '
+        echo (tput setab 1)(prompt_pwd)(tput sgr0)' '(tput setaf 11)'><>'(tput sgr0)' '
 end
